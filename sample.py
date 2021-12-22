9a71c550674d99690444a39f0f8a0aff4375b9f3
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-22 14:37:32 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,13 +3,11 @@ import time
 #author name- sharad Tiwari
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 
-s=Service("/usr/bin/google-chrome")
-
-driver = webdriver.Chrome(service=s)
-#driver = webdriver.Chrome(executable_path="/usr/bin/google-chrome")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path="/usr/bin/google-chrome")
 
 #driver = webdriver.Firefox()
 #driver = webdriver.Ie(executable_path="/usr/bin/google-chrome")
