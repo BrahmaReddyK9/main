8911f23832be9dae9a2febb7b5c95d8072a6fb88
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-22 15:03:26 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Chrome(executable_path="/usr/bin/google-chrome")
 
 #driver = webdriver.Firefox()
 #driver = webdriver.Ie(executable_path=r"C:\Users\Sharad\Desktop\drivers\IEDriverServer.exe")
