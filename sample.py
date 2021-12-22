a7055dc9317238014a2170eaa7bbc643e0fec15b
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-22 13:39:30 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,17 @@
+import time
+
+#author name- sharad Tiwari
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+#driver = webdriver.Firefox()
+#driver = webdriver.Ie(executable_path=r"C:\Users\Sharad\Desktop\drivers\IEDriverServer.exe")
+driver.get("https://www.amazon.com/")
+print(driver.title)
+driver.find_element_by_xpath("//*[@id='nav-link-accountList']/div").click()
+time.sleep(5)
+driver.close()    #currently focused browser
+#driver.quit()     #closes all the browser
