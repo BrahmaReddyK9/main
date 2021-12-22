7a36c47b2bb81d668071cebb32e030b748921675
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-22 18:53:27 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"    #chrome binary location specified here
+options.binary_location = "/usr/bin/chromedriver"    #chrome binary location specified here
 options.add_argument("--start-maximized") #open Browser in maximized mode
 options.add_argument("--no-sandbox") #bypass OS security model
 options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=options, executable_path=r'/usr/bin/google-chrome')
+driver = webdriver.Chrome(options=options, executable_path=r'/usr/bin/chromedriver')
 driver.get('http://google.com/')
